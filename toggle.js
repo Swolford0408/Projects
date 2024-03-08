3c5c568e66119f4aa59c7067c1587943526baff6
Snorlax <vargrskaoi@gmail.com>
2024-03-08 10:19:11 -0600

    Moved the toggle from devDash to toggle.js

--- toggle.js
+++ toggle.js
@@ -1,36 +1,81 @@
-// Changes the card from login to DashBoard and validates input
-
-$('#btnLogin').on('click',function(){
-    // validation function called for Login
-    if(!validateLogin()){
-      $('#divLogin').slideToggle(function(){
-          $('#divDashboard').slideToggle();
-      })
-    }
-});
-
-// Changes the card from Registration to Dashboard and validates input
-$('#btnRegister').on('click',function(){
-    // validation function called for Registration
-    if(!validateRegistration()){
-      $('#divRegister').slideToggle(function(){
-              $('#divDashboard').slideToggle();
-      });
-    }
-});
-
-// Changes between Login and Registration
-$('.btnToggle').on('click',function(){
-    // gets the card the user is currently on
-    let strCard = $(this).attr('data-card');
-    // checks which card the user is on and switches to the other card
-    if(strCard == 'Login'){
-        $('#divLogin').slideToggle(function(){
-            $('#divRegister').slideToggle();
+      $('#btnLogin').on('click',function(){
+            let strEmail = $('#txtLoginUserName').val();
+            let strPassword = $('#txtLoginPassword').val();
+            if(!validateLogin()){
+                $.post('https://simplecoop.swollenhippo.com/sessions.php',{Email:strEmail,Password:strPassword},function(result){
+                    result=JSON.parse(result);
+                    if(result.Outcome != "false" && result.SessionID != "undefine"){
+                        console.log(result);
+                        sessionStorage.setItem("SessionID",result.SessionID)
+                        $('#divLogin').slideToggle(function(){
+                            $('#divDashboard').slideToggle();
+                        })
+                    }else{
+                        console.log(result);
+                        Swal.fire({
+                            icon:'error',
+                            title:'Invalid Credentials',
+                            html:'Your password or username is invalid.'
+                        })
+                    }
+                })
+            }
+        });
+        
+        $('#btnRegister').on('click',function(){
+            let strEmail = $('#txtRegisterEmail').val();
+            let strPassword = $('#txtRegisterPassword').val();
+            let strFirstName = $('#txtRegisterFirstName').val();
+            let strLastName = $('#txtRegisterLastName').val();
+            let strAddressOne = $('#txtRegisterStreetAddress1').val();
+            let strAddressTwo = $('#txtRegisterStreetAddress2').val();
+            let strCity = $('#txtRegisterCity').val();
+            let strState = $('#txtRegisterState').val();
+            let strZip = $('#txtRegisterZIP').val();
+            let strPhone = $('#txtRegisterPhone').val();
+            let strCoopID = $('#txtRegisterCoopID').val();
+            $.post('https://simplecoop.swollenhippo.com/coop.php',function(objCoopID){
+                objCoopID = JSON.parse(objCoopID)
+                console.log(objCoopID);
+                let strCoopID = objCoopID.CoopID;
+            })
+            if(!validateRegistration()){
+                // create user
+                $.post('https://simplecoop.swollenhippo.com/users.php',{Email:strEmail,Password:strPassword,FirstName:strFirstName,LastName:strLastName,CoopID:strCoopID},function(result){
+                    result=JSON.parse(result);
+                    if(!result.error){
+                        console.log(result);
+                        // create Session
+                        $.post('https://simplecoop.swollenhippo.com/sessions.php',{Email:strEmail,Password:strPassword},function(result){
+                            result=JSON.parse(result);
+                            if(!result.error){
+                                console.log(result);
+                                sessionStorage.setItem("SessionID",result.SessionID)
+                                $('#divRegister').slideToggle(function(){
+                                    $('#divDashboard').slideToggle();
+                                });
+                            }else{
+                                console.log(result.error)
+                            }
+                        })
+                    }
+                })
+            }
+        });
+        
+        $('.btnToggle').on('click',function(){
+            let strCard = $(this).attr('data-card');
+            if(strCard == 'Login'){
+                $('#divLogin').slideToggle(function(){
+                    $('#divRegister').slideToggle();
+                })
+                if($(window).width() < 720){
+                    $('#divMain').removeClass('vh-100');
+                }
+            } else {
+                $('#divRegister').slideToggle(function(){
+                    $('#divLogin').slideToggle();
+                })
+                $('#divMain').addClass('vh-100');
+            }
         })
-    } else {
-        $('#divRegister').slideToggle(function(){
-            $('#divLogin').slideToggle();
-        })
-    }
-})
