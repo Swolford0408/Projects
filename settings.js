87ab52395d7c4961b8b8089c39dc581949bdea5d
erikBowling <erik.bowling@outlook.com>
2024-04-08 20:34:54 -0500

    Brainrot

--- settings.js
+++ settings.js
@@ -46,23 +46,68 @@ $('#btnFeedChickens').on('click', function(){
 
 let intervalID = null;
 
+const danceSong = new Audio('./audio_files/chickenDance.mp3');
+const images = [];
 $("#chickenParty").on('click', function(){
     const htmlSettingsCard = document.getElementById("divSettings");
 
-    const danceSong = new Audio('./audio_files/chickenDance.mp3');
-
+    if(images.length > 0){
+        images.forEach((img)=>{
+            img.remove();
+        })
+    }
     if(!danceSong.paused){
         danceSong.pause();
         clearInterval(intervalID);
     }else{
         danceSong.play();
+        
+        const numImages = 10;
+        
+        // Loop to create and position images
+        for (let i = 0; i < numImages; i++) {
+            // Create new image element
+            const img = new Image();
+            img.src = "./images/chicken.png"; // Set image source
+    
+            // Get random position
+            const pos = getRandomPosition();
+
+            // Set image position
+            img.style.width = '200px'
+            img.style.height = '200px'
+            img.style.left = pos[0] + 'px';
+            img.style.top = pos[1] + 'px';
+            img.style.position = 'absolute';
+            img.style.transform = `rotate(${Math.random() * 360} deg)`;
+    
+            // Append image to the body
+            document.body.appendChild(img);
+            images.push(img);
+        }
+
         intervalID = setInterval(()=>{
             party.confetti(htmlSettingsCard, {
                 count: party.variation.range(20, 40)
             })
+
+            for(let i = 0; i < images.length; i++){
+                const pos = getRandomPosition();
+                images[i].style.left = pos[0] + 'px';
+                images[i].style.top = pos[1] + 'px';
+
+            }
+            
         }, 1000)
     }
 })
+
+function getRandomPosition() {
+    var x = Math.floor(Math.random() * window.innerWidth);
+    var y = Math.floor(Math.random() * window.innerHeight);
+    return [x, y];
+}
+
 
 // Used to quickly login to save development time
 $('#btnDevLogin').on('click', function(){
