9353e83dab783c149d61066fe39b2ff0f16fa321
erikBowling <erik.bowling@outlook.com>
2024-04-08 20:11:17 -0500

    Settings | Chicken Dance

--- settings.js
+++ settings.js
@@ -1,20 +1,70 @@
-$('.btnSettingsToggle').on('click',function(){
-    let strCard = $(this).attr('data-card');
-    if(strCard == 'Login'){
-        $('#divLogin').slideToggle(function(){
-            $('#divRegister').slideToggle();
+// Toggle to show settings card
+
+$('#btnSettingsToggle').on('click',function(){
+    const strCard = $(this).attr('data-card');
+    if(strCard === 'dashboard'){
+        $('#divSettings').slideToggle(function(){
+            $('#divDashboard').slideToggle();
         })
-        if($(window).width() < 720){
-            $('#divMain').removeClass('vh-100');
-        }
     } else {
-        $('#divRegister').slideToggle(function(){
-            $('#divLogin').slideToggle();
+        $('#divDashboard').slideToggle(function(){
+            $('#divSettings').slideToggle();
         })
-        $('#divMain').addClass('vh-100');
     }
 })
 
+// Used to update the fan speed
+$(document).on('input', '#rangeFanSpeed', function() {
+    $('#spanFanSpeed').html( `${$(this).val()} RPM` );
+});
+
+// Used to see the current light status (on / off)
+$('input[name="btnLightRadio"]').change(function() {
+    const activeRadioValue = $(this).val();
+    console.log("Active radio button value: " + activeRadioValue);
+});
+
+
+$('#btnFeedChickens').on('click', function(){
+    // let storedTime = '';
+    // $.getJSON(strSettingsUrl, postObj, (result)=>{
+    // })  
+    const strSettingsUrl = 'https://simplecoop.swollenhippo.com/settings.php';
+
+    const postObj = {
+        SessionID:sessionStorage.getItem('SessionID'), 
+        setting: 'TimeFed',
+        value: `${Date.now()}`
+    }
+
+    console.log(postObj);
+
+    $.post(strSettingsUrl, postObj, (result)=>{
+        console.log(result);
+    });
+});
+
+let intervalID = null;
+
+$("#chickenParty").on('click', function(){
+    const htmlSettingsCard = document.getElementById("divSettings");
+
+    const danceSong = new Audio('./audio_files/chickenDance.mp3');
+
+    if(!danceSong.paused){
+        danceSong.pause();
+        clearInterval(intervalID);
+    }else{
+        danceSong.play();
+        intervalID = setInterval(()=>{
+            party.confetti(htmlSettingsCard, {
+                count: party.variation.range(20, 40)
+            })
+        }, 1000)
+    }
+})
+
+// Used to quickly login to save development time
 $('#btnDevLogin').on('click', function(){
     createSession("coop@example.com", "ASD123asd", "login");
 })
