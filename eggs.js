fafb5e9543b78eb970b03bcc660ced7354bd8da8
Anekin Kryter <160093849+anekinj@users.noreply.github.com>
2024-04-17 17:38:16 -0500

    Getting table working

--- eggs.js
+++ eggs.js
@@ -11,10 +11,56 @@ function getRandomObservationDatetime() {
     return observationDatetime;
 }
 
-// use $('#numTotalEggs').text(someNumber); to change <p>
+function deleteEggs(){
+    let successDelete = false;
+    $.ajax({
+        url:'https://simplecoop.swollenhippo.com/eggs.php',
+        data:{SessionID:sessionStorage.getItem('SessionID')},
+        type: 'DELETE',
+        success: function(result){
+            console.log(result);
+            successDelete = true;
+        }
+    })
+    return successDelete;
+}
+
+function getEggCount() {
+    let intEggCount = 0;
+
+    $.getJSON('https://simplecoop.swollenhippo.com/eggs.php', {SessionID:sessionStorage.getItem('SessionID'), days:100}, function(result) {
+        console.log(result);
+
+        result.forEach(function(eggRecord, index) {
+            intEggCount += eggRecord.Harvested;
+        })
+    })
+
+    console.log(intEggCount);
+
+    return intEggCount;
+}
+
+function getEggTable() {
+    $.getJSON('https://simplecoop.swollenhippo.com/eggs.php', {SessionID:sessionStorage.getItem('SessionID'), days:100}, function(result) {
+        result.forEach(function(eggRecord) {
+            $('#tblEgg tbody').append('<tr><td>' + eggRecord.LogDateTime + '</td><td>' + eggRecord.Harvested + '</td><td><button class="btn btn-danger col-12 btnDeleteEgg" type="button" data-search="' + eggRecord.LogID + '">Delete</button></td></tr>');
+        })
+    })
+
+    $(document).ready(function(){
+        $('#tblEnv').dataTable();
+    });
+}
 
 $.getJSON('https://simplecoop.swollenhippo.com/eggs.php', {SessionID:sessionStorage.getItem('SessionID'), days:100}, function(result) {
-    console.log(result);
+    let intEggCount = 0;
+
+    result.forEach(function(eggRecord, index) {
+        intEggCount += eggRecord.Harvested;
+    })
+
+    $('#numTotalEggs').text(intEggCount);
 })
 
 $('#btnHarvest').on('click', function() {
@@ -26,4 +72,47 @@ $('#btnHarvest').on('click', function() {
             console.log(result);
         })
     }
+
+    $.getJSON('https://simplecoop.swollenhippo.com/eggs.php', {SessionID:sessionStorage.getItem('SessionID'), days:100}, function(result) {
+        console.log(result);
+        let intEggCount = 0;
+
+        result.forEach(function(eggRecord, index) {
+            intEggCount += eggRecord.Harvested;
+        })
+
+        $('#numTotalEggs').text(intEggCount);
+    })
+})
+
+$('#btnRefreshEgg').on('click', function() {
+    $.getJSON('https://simplecoop.swollenhippo.com/eggs.php', {SessionID:sessionStorage.getItem('SessionID'), days:100}, function(result) {
+        console.log(result);
+        let intEggCount = 0;
+
+        result.forEach(function(eggRecord, index) {
+            intEggCount += eggRecord.Harvested;
+        })
+
+        $('#numTotalEggs').text(intEggCount);
+    })
+
+    $('#tblEgg tbody tr').remove();
+    getEggTable();
+})
+
+$(document).on('click', '.btnDeleteEgg', function() {
+    let strLogID = $(this).attr('data-search');
+
+    $.ajax({
+        url:'https://simplecoop.swollenhippo.com/eggs.php',
+        data:{SessionID:sessionStorage.getItem('SessionID'), logID:strLogID},
+        type:'DELETE',
+        success:function(result) {
+            console.log(result);
+
+            $('#tblEgg tbody tr').remove();
+            getEggTable();
+        }
+    })
 })
