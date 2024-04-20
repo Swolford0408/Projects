c20b18b79e70f067949047c04271351fd8cc658e
erikBowling <erik.bowling@outlook.com>
2024-04-19 20:53:21 -0500

    TImer and lights on or off done.

--- settings.js
+++ settings.js
@@ -1,5 +1,4 @@
 // Toggle to show settings card
-
 $('#btnSettingsToggle').on('click',function(){
     const strCard = $(this).attr('data-card');
     if(strCard === 'dashboard'){
@@ -11,24 +10,41 @@ $('#btnSettingsToggle').on('click',function(){
             $('#divSettings').slideToggle();
         })
     }
+});
+
+// Used to quickly login to save development time
+$('#btnDevLogin').on('click', function(){
+    createSession("coop@example.com", "ASD123asd", "login");
 })
+
+// Erik Bowling
+// Smart Coop
 
 // GLOBAL VARIABLES
 const strSettingsUrl = 'https://simplecoop.swollenhippo.com/settings.php';
-let intLastTimeChickensFed = null;
+var intLastTimeChickensFed = null;
 
-// SETUP Variables
+/////////////////////////////////////////////
+// FEEDING and TIMER
+/////////////////////////////////////////////
+
 $.getJSON(strSettingsUrl, {SessionID: sessionStorage.getItem('SessionID'), setting: 'TimeFed'}, (result)=>{
     intLastTimeChickensFed = Number(result.Value);
-})
+    const intTimeLeft = intLastTimeChickensFed + 600000 - Date.now();
 
-// If it has been at least ten minutes since last time.
-if(Number(intLastTimeChickensFed) + 600000 < Date.now()){
-    $('#btnFeedChickens').removeClass('d-none');
-}else{
-    
-}
+    // If it has been less than 10 minutes, start the timer.
 
+    if(intTimeLeft > 0){
+        $('#btnFeedChickens').hide();
+        $('#paraFeedTime').show();
+        createTimer(intTimeLeft);
+    }else{
+        $('#btnFeedChickens').show();
+        $('#paraFeedTime').hide();
+    }
+});
+
+// Used to create the setInterval to count down when you can feed chickens again.
 const createTimer = (numTimeinMiliSec)=>{
 
     const timeLeftObj = {
@@ -36,6 +52,7 @@ const createTimer = (numTimeinMiliSec)=>{
         min: Math.floor(numTimeinMiliSec / 60000)
     }
 
+    // Timer with clock like behavior
     const timer = setInterval(()=>{
         timeLeftObj.sec--;
 
@@ -45,42 +62,79 @@ const createTimer = (numTimeinMiliSec)=>{
         }
 
         if(timeLeftObj.min < 0){
-            $()
+            $('#btnFeedChickens').show();
+            $('#paraFeedTime').hide();
             clearInterval(timer);
         }else{
             $('#paraFeedTime').html(`${timeLeftObj.min < 10 ? '0'+timeLeftObj.min : timeLeftObj.min }:${timeLeftObj.sec < 10 ? '0'+timeLeftObj.sec : timeLeftObj.sec}`)
         }
-    }, 1000)
+    }, 1000);
 
-    return timer
+    return timer;
 }
 
-const feedChickens = ()=>{
-    const timer = createTimer(60000);
+$('#btnFeedChickens').on('click', function(){
+    $('#btnFeedChickens').slideToggle(function(){
+        $('#paraFeedTime').slideToggle();
+    })
+    $('#paraFeedTime').html('10:00');
+    createTimer(600000);
+    const objPutParams = {
+        SessionID: sessionStorage.getItem('SessionID'), 
+        setting: 'TimeFed', 
+        value: Date.now()
+    }
 
-    console.log(timer);
-}
+    $.ajax({
+        url: strSettingsUrl,
+        type: 'PUT',
+        data: objPutParams,
+        success: function(result){
+            console.log(result)
+        }
+    });
+});
 
-// Used to update the fan speed
+/////////////////////////////////////////////
+// FAN SPEED
+/////////////////////////////////////////////
+
 $(document).on('input', '#rangeFanSpeed', function() {
     $('#spanFanSpeed').html( `${$(this).val()} RPM` );
 });
 
-// Used to see the current light status (on / off)
-$('input[name="btnLightRadio"]').change(function() {
-    const activeRadioValue = $(this).val();
-    console.log("Active radio button value: " + activeRadioValue);
+/////////////////////////////////////////////
+// LIGHTS ON OR OFF
+/////////////////////////////////////////////
+
+// Turn the lights back to the way you last left them
+$.getJSON(strSettingsUrl, {SessionID: sessionStorage.getItem('SessionID'), setting: 'LightsOnOff'}, (result)=>{
+    const strLightStatus = result.Value;
+
+    if(strLightStatus == "Off"){
+        $('#btnToggleLightsOff').attr('checked', true)
+    }else{
+        $('#btnToggleLightsOn').attr('checked', true)
+    }
 });
 
-$('#btnFeedChickens').on('click', feedChickens)
+// Click of the radio on or off.
+$('input[name="btnLightRadio"]').change(function() {
+    const activeRadioValue = $(this).val();
 
-$("#chickenParty").on('click', function(){
-    
-    
-})
+    const objPutParams = {
+        SessionID: sessionStorage.getItem('SessionID'), 
+        setting: 'LightsOnOff', 
+        value: activeRadioValue
+    }
 
+    $.ajax({
+        url: strSettingsUrl,
+        type: 'PUT',
+        data: objPutParams,
+        success: function(result){
+            console.log(result)
+        }
+    });
 
-// Used to quickly login to save development time
-$('#btnDevLogin').on('click', function(){
-    createSession("coop@example.com", "ASD123asd", "login");
-})
+});
