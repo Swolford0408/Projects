d72c0bc973443e5ba9603a2000cfd22d0f338b34
Vincent Lin <vincentlin0117@gmail.com>
2024-04-21 14:36:48 -0500

    fixed validate session and created getSessions function

--- src/js/sessions.js
+++ src/js/sessions.js
@@ -1,37 +1,52 @@
 // auto login if Session ID still exist
-$.getJSON('https://simplecoop.swollenhippo.com/sessions.php',{SessionID:sessionStorage.getItem('SessionID')},function(session){
-    console.log(session);
+getSession(function(session){
     if(session != null && validateSession(session)){
         $('#divStart').slideUp(function(){
             $('#divDashBoard').slideDown();
             $('.navbar').slideDown();
         })
     }else{
-        $('#btnLogOut').click();
+        $('#liLogout').click();
     }
 })
 
+// get sessions object
+function getSession(callback){
+    $.getJSON('https://simplecoop.swollenhippo.com/sessions.php',{SessionID:sessionStorage.getItem('SessionID')},function(session){
+        callback(session)
+    })
+}
+
+// check if session is valid or not, 
 function validateSession(session){
     // set current datetime
     let currentDate = new Date();
-    
+
     // set start datetime
     let sessionStartParts = session.StartDateTime.split(/[- :]/)
-    let sessionStartDate = new Date(sessionStartParts[0], sessionStartParts[1] - 1, sessionStartParts[2], sessionStartParts[3], sessionStartParts[4], sessionStartParts[5]);
+    let sessionStartDate = new Date(sessionStartParts[0], sessionStartParts[1] - 1, sessionStartParts[2], sessionStartParts[3]-1, sessionStartParts[4], sessionStartParts[5]);
     
     // check if the date is the same and if it has been 12 hours since they logged in
-    if ((currentDate.getTime() - sessionStartDate.getTime())/ (1000 * 60 * 60) <= 12){
+    if ((currentDate.getTime()-  sessionStartDate.getTime())/ (1000 * 60 * 60) <= 12){
         // set active datetime
         let sessionUsedParts = session.LastUsedDateTime.split(/[- :]/)
-        let sessionUsedDate = new Date(sessionUsedParts[0], sessionUsedParts[1] - 1, sessionUsedParts[2], sessionUsedParts[3], sessionUsedParts[4], sessionUsedParts[5]);
-        
+        let sessionUsedDate = new Date(sessionUsedParts[0], sessionUsedParts[1] - 1, sessionUsedParts[2], sessionUsedParts[3]-1, sessionUsedParts[4], sessionUsedParts[5]);
+        console.log(currentDate)
+        console.log(sessionUsedDate)
+        console.log((currentDate.getTime() - sessionUsedDate.getTime())/ (1000 * 60))
+        console.log(currentDate.getTime())
+        console.log(sessionUsedDate.getTime())
         // if it has not been 15 min update date time
-        if((currentDate.getTime() - sessionUsedDate.getTime())/ (1000 * 60) >= 15){
+        if((currentDate.getTime() - sessionUsedDate.getTime())/ (1000 * 60) <= 1){
+            console.log((currentDate.getTime() - sessionUsedDate.getTime())/ (1000 * 60))
+            console.log(currentDate.getTime())
+            console.log(sessionUsedDate.getTime())
             updateSession();
             return true;
         }
     }
     // return false if it is expired
+    console.log("here")
     return false;
     
 }
