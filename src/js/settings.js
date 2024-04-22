fc710841feed4b66cac6b7ab7f146da049538519
erikBowling <erik.bowling@outlook.com>
2024-04-21 21:52:58 -0500

    Timer explanation | dark mode on load if in db

--- src/js/settings.js
+++ src/js/settings.js
@@ -44,7 +44,7 @@ const createTimer = (numTimeinMiliSec)=>{
 
         if(timeLeftObj.min < 0){
             $('#btnFeedChickens').show();
-            $('#paraFeedTime').hide();
+            $('#feedOrNoFeed').hide();
             clearInterval(timer);
         }else{
             $('#paraFeedTime').html(`${timeLeftObj.min < 10 ? '0'+timeLeftObj.min : timeLeftObj.min }:${timeLeftObj.sec < 10 ? '0'+timeLeftObj.sec : timeLeftObj.sec}`)
@@ -60,7 +60,7 @@ $('#btnFeedChickens').on('click', function(){
             $('#liLogout').click();
         }else{
             $('#btnFeedChickens').slideToggle(function(){
-                $('#paraFeedTime').slideToggle();
+                $('#feedOrNoFeed').slideToggle();
             })
         
             $('#paraFeedTime').html('10:00');
@@ -107,6 +107,8 @@ $.getJSON(strSettingsUrl, {SessionID: sessionStorage.getItem('SessionID'), setti
     }else{
         $('#btnToggleLightsOn').attr('checked', true)
     }
+
+    turnOffLights(strLightStatus);
 });
 
 // Click of the radio on or off.
@@ -131,73 +133,77 @@ $('input[name="btnLightRadio"]').change(function() {
                 }
             });
             
-            if(activeRadioValue == "Off"){
-                $(".bg-yellow").each(function(){
-                    $(this).removeClass('bg-yellow');
-                    $(this).addClass('bg-dark-yellow');
-                });
-
-                $(".bg-red").each(function(){
-                    $(this).removeClass('bg-red');
-                    $(this).addClass('bg-dark-red');
-                });
-
-                $(".bg-white").each(function(){
-                    $(this).removeClass('bg-white');
-                    $(this).addClass('bg-dark-white');
-                });
-
-                $(".border-white").each(function(){
-                    $(this).removeClass('border-white');
-                    $(this).addClass('border-dark-white');
-                });
-
-                $(".outline-red").each(function(){
-                    $(this).removeClass('outline-red');
-                    $(this).addClass('outline-dark-red');
-                });
-
-                $(".outline-yellow").each(function(){
-                    $(this).removeClass('outline-yellow');
-                    $(this).addClass('outline-dark-rellow');
-                });
-
-                $("#mainContainerDiv").removeClass('daytime-bg-image');
-                $("#mainContainerDiv").addClass('nighttime-bg-image');
-            }else{
-                $(".bg-dark-yellow").each(function(){
-                    $(this).removeClass('bg-dark-yellow');
-                    $(this).addClass('bg-yellow');
-                });
-            
-                $(".bg-dark-red").each(function(){
-                    $(this).removeClass('bg-dark-red');
-                    $(this).addClass('bg-red');
-                });
-            
-                $(".bg-dark-white").each(function(){
-                    $(this).removeClass('bg-dark-white');
-                    $(this).addClass('bg-white');
-                });
-            
-                $(".border-dark-white").each(function(){
-                    $(this).removeClass('border-dark-white');
-                    $(this).addClass('border-white');
-                });
-            
-                $(".outine-dark-red").each(function(){
-                    $(this).removeClass('outline-dark-red');
-                    $(this).addClass('outline-red');
-                });
-            
-                $(".outine-dark-yellow").each(function(){
-                    $(this).removeClass('outline-dark-yellow');
-                    $(this).addClass('outline-yellow');
-                });
-
-                $("#mainContainerDiv").removeClass('nighttime-bg-image');
-                $("#mainContainerDiv").addClass('daytime-bg-image');
-            }
+            turnOffLights(activeRadioValue);
         }
     })
 });
+
+const turnOffLights = (activeRadioValue)=>{
+    if(activeRadioValue == "Off"){
+        $(".bg-yellow").each(function(){
+            $(this).removeClass('bg-yellow');
+            $(this).addClass('bg-dark-yellow');
+        });
+
+        $(".bg-red").each(function(){
+            $(this).removeClass('bg-red');
+            $(this).addClass('bg-dark-red');
+        });
+
+        $(".bg-white").each(function(){
+            $(this).removeClass('bg-white');
+            $(this).addClass('bg-dark-white');
+        });
+
+        $(".border-white").each(function(){
+            $(this).removeClass('border-white');
+            $(this).addClass('border-dark-white');
+        });
+
+        $(".outline-red").each(function(){
+            $(this).removeClass('outline-red');
+            $(this).addClass('outline-dark-red');
+        });
+
+        $(".outline-yellow").each(function(){
+            $(this).removeClass('outline-yellow');
+            $(this).addClass('outline-dark-rellow');
+        });
+
+        $("#mainContainerDiv").removeClass('daytime-bg-image');
+        $("#mainContainerDiv").addClass('nighttime-bg-image');
+    }else{
+        $(".bg-dark-yellow").each(function(){
+            $(this).removeClass('bg-dark-yellow');
+            $(this).addClass('bg-yellow');
+        });
+    
+        $(".bg-dark-red").each(function(){
+            $(this).removeClass('bg-dark-red');
+            $(this).addClass('bg-red');
+        });
+    
+        $(".bg-dark-white").each(function(){
+            $(this).removeClass('bg-dark-white');
+            $(this).addClass('bg-white');
+        });
+    
+        $(".border-dark-white").each(function(){
+            $(this).removeClass('border-dark-white');
+            $(this).addClass('border-white');
+        });
+    
+        $(".outine-dark-red").each(function(){
+            $(this).removeClass('outline-dark-red');
+            $(this).addClass('outline-red');
+        });
+    
+        $(".outine-dark-yellow").each(function(){
+            $(this).removeClass('outline-dark-yellow');
+            $(this).addClass('outline-yellow');
+        });
+
+        $("#mainContainerDiv").removeClass('nighttime-bg-image');
+        $("#mainContainerDiv").addClass('daytime-bg-image');
+    }
+}
